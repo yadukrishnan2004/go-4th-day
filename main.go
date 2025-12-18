af6f99aa467c6942cece523606f739cd3b2979d7
yadhukrishnan2004 <yadhukrishnangta@gmail.com>
2025-12-18 12:57:18 +0530

    gorm with ecommerce_db

--- main.go
+++ main.go
@@ -2,97 +2,95 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	"net/http"
 )
 
-type Users struct {
+type users struct {
 	gorm.Model
 	NAME  string `gorm:"not null" json:"name" binding:"required,min=2"`
-	EMAIL string `gorm:"not null;unique" json:"email" binding:"required,email"`
+	EMAIL string `gorm:"not null" json:"email" binding :"required,email"`
 }
 
 var db *gorm.DB
-var err error
+var err1 error
 
 func main() {
-	dsn := "host=localhost user=postgres password=1234 dbname=ecommerce_db port=5432 sslmode=disable "
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to DB:", err)
+	dsn := "host=localhost user=postgres password=1234 dbname=ecommerce_db port=5432 sslmode=disable"
+	db, err1 = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err1 != nil {
+		log.Println("error during connecting the db", err1)
 	}
-	db.AutoMigrate(&Users{})
+	if err := db.AutoMigrate(&users{}); err != nil {
+		log.Println("auto migration fail")
+		return
+	}
 	fmt.Println("auto migrate connected")
 
-	r := gin.Default()
-	r.GET("", Allusers)
-	r.POST("", postuser)
-	r.PUT("/:id", updateuser)
-	r.DELETE("/:id", deleteuser)
+	r:=gin.Default()
+	v1:=r.Group("/v1")
+	{
+		v1.GET("/",GetUsers)
+		v1.POST("/",CreateUser)
+		v1.PUT("/:id",updateuser)
+		v1.DELETE("/:id",deleteUser)
+	}
 
 	r.Run(":8080")
+}
+func GetUsers(c *gin.Context){
+	var user users
+	result:=db.Find(&user)
+	if result.Error != nil{
+		c.JSON(400,gin.H{
+			"message":result.Error.Error(),
+		})
+		c.JSON(200,user)
+	}
 
 }
-func Allusers(c *gin.Context) {
-	var user []Users
-	result := db.Find(&user)
-	if result.Error != nil {
-		c.JSON(400, gin.H{
-			"error": "something went wrong",
-		})
-		return
+
+func CreateUser (c *gin.Context){
+	var user users
+	if err:=c.ShouldBindJSON(&user); err !=nil{
+		c.JSON(400,gin.H{"error":"something not right on the input"})
 	}
-	c.JSON(http.StatusOK, user)
+	result:=db.Create(&user)
+	if result.Error !=nil {
+		c.JSON(400,gin.H{"message":"something not right"})
+	}
+	c.JSON(200,&user)
 }
-func postuser(c *gin.Context) {
-	var user Users
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, err.Error())
-		return
+
+func updateuser (c *gin.Context){
+	var user,input users
+	id:=c.Param("id")
+	result:=db.Find(&user,id)
+	if result.Error != nil{
+		c.JSON(400,gin.H{"message":"user not found"})
 	}
-	result := db.Create(&user)
-	if result.Error != nil {
-		c.JSON(400, result.Error.Error())
-		return
+	if err:=c.ShouldBindJSON(&input); err != nil{
+		c.JSON(400,gin.H{"error":"error"})
 	}
-	c.JSON(200, user)
+	results:=db.Model(&user).Updates(&input)
+	if results.Error != nil{
+		c.JSON(400,results.Error.Error())
+	}
+	c.JSON(200,input)
 }
-func updateuser(c *gin.Context) {
-	var user Users
-	var input Users
-	id := c.Param("id")
-	result := db.Find(&user, id)
-	if result.Error != nil {
-		c.JSON(400, result.Error.Error())
-		return
+
+func deleteUser(c *gin.Context){
+	id:=c.Param("id")
+	result:=db.Delete(&users{},id)
+	if result.Error != nil{
+		c.JSON(400,gin.H{"message":"user not found"})
 	}
-	err := c.ShouldBindJSON(&input)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message": err,
-		})
-		return
-	}
-	errr := db.Model(&user).Updates(input)
-	if errr.Error != nil {
-		c.JSON(400, gin.H{
-			"message": errr.Error.Error(),
-		})
-		return
-	}
-	c.JSON(200, user)
+	c.JSON(200,gin.H{"message":"user deleted"})
 }
-func deleteuser(c *gin.Context) {
-	id := c.Param("id")
-	result := db.Unscoped().Delete(&Users{}, id)
-	if result.Error != nil {
-		c.JSON(400, result.Error.Error())
-		return
-	}
-	c.JSON(200, gin.H{
-		"message": "user deleted",
-	})
-}
+
+
+
+
